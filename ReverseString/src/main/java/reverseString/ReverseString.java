ed0ce69bc6aaff48f55391828a6456967e5a5b68
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-12-10 14:34:11 +0100

    exercise

--- ReverseString/src/main/java/reverseString/ReverseString.java
+++ ReverseString/src/main/java/reverseString/ReverseString.java
@@ -4,8 +4,13 @@ import static reverseString.Reverse.reverse;
 
 public class ReverseString {
 
-    public static void main(String[]args){
-        String r = reverse("dog, kayak, Pawel");
-        System.out.println(r);
+    public static void main(String[] args) {
+        String r = reverse("dog, kayak, Pawel");{
+        if (r.startsWith("k")){
+                System.out.println("it is correct");
+            } else {
+                System.out.println("not correct");
+            }
+        }
     }
 }
